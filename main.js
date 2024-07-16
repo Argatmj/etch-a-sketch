78b756d114c8c588734b73736b0984ea2908b572
Argatmj <ikomangarga0612@gmail.com>
2024-07-16 15:28:21 +0200

    Added color, eraser, and clear functionality

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function createGrid(num) {
     let container = document.querySelector(".container");
-
     let containerWidth = 400; 
     let containerHeight = 400; 
 
@@ -12,16 +11,34 @@ function createGrid(num) {
     for (let i = 0; i < num * num; i++) {
         let cell = document.createElement("div");
         cell.style.margin = 0;
+        cell.className = "cells";
         cell.style.backgroundColor = "white";
         cell.style.boxSizing = "border-box";
-        
         cell.style.width = `${cellSize}px`;
         cell.style.height = `${cellSize}px`;
+
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = selectedColor;
+        });
 
         container.appendChild(cell);
     }
 }
 
+let colourBtn = document.querySelector(".select");
+let clearBtn = document.querySelector("#clear");
+let eraseBtn = document.querySelector("#erase");
+let selectedColor = '';
 
+eraseBtn.addEventListener("click", () => selectedColor = "white");
 
-createGrid(10);
+colourBtn.addEventListener("change", () => selectedColor = colourBtn.value);
+
+clearBtn.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cells");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = "white";
+    });
+});
+
+createGrid(30);
