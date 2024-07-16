5db085e3cbd0bc63de3f82d47d2050c08180cfa0
Argatmj <ikomangarga0612@gmail.com>
2024-07-16 16:44:11 +0200

    Added rainbow-mode and darken-mode

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function createGrid(num) {
     for (let i = 0; i < num * num; i++) {
         let cell = document.createElement("div");
         cell.style.margin = 0;
+        cell.style.opacity = 1;
         cell.className = "cells";
         cell.style.backgroundColor = "white";
         cell.style.boxSizing = "border-box";
@@ -18,18 +19,33 @@ function createGrid(num) {
         cell.style.height = `${cellSize}px`;
 
         cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = selectedColor;
+            if(!darkens){
+                cell.style.opacity = 1;
+                cell.style.backgroundColor = random ? `#${getRandomColor()}` : selectedColor;
+            }
+            else{
+                let currentOpac = cell.style.opacity;
+                cell.style.opacity = currentOpac - 0.1;
+            }
         });
 
         container.appendChild(cell);
     }
 }
 
+function getRandomColor(){
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
 let colourBtn = document.querySelector(".select");
 let sizeBtn = document.querySelector(".size");
 let clearBtn = document.querySelector("#clear");
+let rbBtn = document.querySelector("#rainbow");
 let eraseBtn = document.querySelector("#erase");
+let opacBtn = document.querySelector("#opac");
 let selectedColor = '';
+let random = false;
+let darkens = false;
 
 sizeBtn.addEventListener("mousemove",() => {
     let nums = document.querySelectorAll(".num");
@@ -44,14 +60,30 @@ sizeBtn.addEventListener("change", () => {
     createGrid(sizeBtn.value);
 });
 
-eraseBtn.addEventListener("click", () => selectedColor = "white");
+rbBtn.addEventListener("click",() => random = true);
 
-colourBtn.addEventListener("change", () => selectedColor = colourBtn.value);
+opacBtn.addEventListener("click", () => {
+    random = false;    
+    darkens = true
+});
+
+eraseBtn.addEventListener("click", () => {
+    random = false;
+    darkens = false;
+    selectedColor = "white"
+});
+
+colourBtn.addEventListener("change", () => {
+    random = false; 
+    darkens = false;
+    selectedColor = colourBtn.value
+});
 
 clearBtn.addEventListener("click", () => {
     const cells = document.querySelectorAll(".cells");
     cells.forEach(cell => {
         cell.style.backgroundColor = "white";
+        cell.style.opacity = 1;
     });
 });
 
